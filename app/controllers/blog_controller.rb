2835b01cd9730f389c49100f9ba9d0f24fc70515
Daniel Torres <denial.torres@gmail.com>
2021-12-22 18:37:13 -0600

    setting up the blog singlenton in an initializer

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,12 +1,4 @@
 class BlogController < ApplicationController
   def index
-    post1 = @blog.new_post
-    post1.title = "Paint just applied"
-    post1.body = "Paint just applied. It's a lovely orangey-purple"
-    post1.publish
-
-    post2 = @blog.new_post(title: 'Still Wet')
-    post2.body = "Paint is still quite wet, not bubbling yet!"
-    post2.publish
   end
 end
