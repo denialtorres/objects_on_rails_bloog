309db72c5cdef54cf26023d3e058fdc3c84dd1c1
Daniel Torres <denial.torres@gmail.com>
2021-12-23 16:00:32 -0600

    rspecs for blog sort and limit

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -32,6 +32,37 @@ RSpec.describe Blog do
     end
   end
 
+  describe "#entries" do
+    def stub_entry_with_date(date)
+      OpenStruct.new(pubdate: DateTime.parse(date))
+    end
+
+    it 'is sorted in reverse-cronological order' do
+      oldest = stub_entry_with_date("2011-09-09")
+      newest = stub_entry_with_date("2011-09-11")
+      middle = stub_entry_with_date("2011-09-10")
+
+      @it.add_entry(oldest)
+      @it.add_entry(newest)
+      @it.add_entry(middle)
+
+      expect(@it.entries).to eq([newest, middle, oldest])
+    end
+
+    it 'is limited to 10 items' do
+      10.times do |i|
+        @it.add_entry(stub_entry_with_date("2011-09-#{i + 1}"))
+      end
+
+      oldest = stub_entry_with_date("2011-08-30")
+
+      @it.add_entry(oldest)
+
+      expect(@it.entries.size).to eq(10)
+      expect(@it.entries).not_to include(oldest)
+    end
+  end
+
   describe "#add_entry" do
     it "adds the entry to the blog" do
       entry = Object.new
